581f867f120ff8c91c093eaf330ae1eaef0e622b
josephlaotzu <josephlao.1398@gmail.com>
2020-03-31 23:13:35 -0400

    update send message form UI

--- client/src/components/ChatBox.js
+++ client/src/components/ChatBox.js
@@ -7,7 +7,7 @@ import { SendForm } from "./SendForm";
 const StledChatBoxSection = styled.section`
   background-color: white;
   display: grid;
-  grid-template-rows: auto 4em;
+  grid-template-rows: auto 6em;
   min-height: calc(100vh - 3.5em);
 `;
 
